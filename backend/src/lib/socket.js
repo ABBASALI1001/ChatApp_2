1c134c6c4442afb984b1e61f20cb47a603f18312
mabbascentennialinfotech-gif <mabbas.centennialinfotech@gmail.com>
2026-06-17 23:12:14 +0530

    package.json file

--- backend/src/lib/socket.js
+++ backend/src/lib/socket.js
@@ -5,9 +5,11 @@ import express from "express";
 const app = express();
 const server = http.createServer(app);
 
+// ✅ Update CORS for production
 const io = new Server(server, {
   cors: {
-    origin: ["http://localhost:5173"],
+    origin: process.env.CLIENT_URL || "http://localhost:5173",
+    credentials: true,
   },
 });
 
